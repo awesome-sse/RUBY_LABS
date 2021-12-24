bf7d4df9554e4a4fe699118a536c3f5258cc1107
awesome-sse <sse103@mail.ru>
2021-12-24 20:23:09 +0300

    Use rubocop

--- test.rb
+++ test.rb
@@ -3,14 +3,13 @@
 require 'minitest/autorun'
 require_relative 'functions'
 
-
 class TestRobot < Minitest::Test
   def test_y
-      assert_in_delta(140.001903, calc_y(5), 0.00001) 
+    assert_in_delta(140.001903, calc_y(5), 0.00001)
   end
 
   def test_correction_str
-    str = "Hel^lo 2wor_ld!"
+    str = 'Hel^lo 2wor_ld!'
     assert_equal ['Hello _world', 4], correction_str(str)
   end
 end
